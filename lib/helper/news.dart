ea86d9b9fa66ce00951db5bc310c24b3aed37c25
Hritvik3 <56683061+Hritvik3@users.noreply.github.com>
2020-11-08 11:52:18 +0530

    Add files via upload

---
+++ lib/helper/news.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:flut_news/model/article_model.dart';
+import 'package:http/http.dart' as http;
+
+class News {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=in&apiKey=cb66913ee7ba40ab883db28edc687f13";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData['articles'].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              url: element['url'],
+              urlToImage: element['urlToImage'],
+              content: element['content']);
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
+
+class CategoryNewsClass {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews(String category) async {
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=cb66913ee7ba40ab883db28edc687f13";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData['articles'].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              url: element['url'],
+              urlToImage: element['urlToImage'],
+              content: element['content']);
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
